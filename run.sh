059e26faebcbc42260cf64576b1bdb22089cdcbd
Jacob Sun <jacobsun000@gmail.com>
2024-07-06 00:31:58 -0500

    chore: Install & Run script

--- run.sh
+++ run.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+echo "Running yarn install in the root directory..."
+yarn install
+
+echo "Running yarn install in the ./frontend directory..."
+cd frontend
+yarn install
+cd ..
+
+echo "Running yarn install in the ./backend directory..."
+cd backend
+yarn install
+cd ..
+
+echo "Yarn install completed in all specified directories."
+
+echo "Running yarn start in the root directory..."
 yarn start
