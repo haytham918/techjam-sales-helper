8415a91461ac3ac26f46166e770ae1469959d924
Jacob Sun <jacobsun000@gmail.com>
2024-07-06 00:22:03 -0500

    fix: ProductCard display

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 import './ProductCard.css';
 
 /**
@@ -11,30 +11,47 @@ import './ProductCard.css';
  * @param {Object} props.product The product object containing details.
  * @param {number} props.product.id The unique identifier for the product.
  * @param {string} props.product.name The name of the product.
- * @param {string} props.product.imageUrl The URL of the product image.
+ * @param {string} props.product.image_url The URL of the product image.
+ * @param {string} props.product.link_url The URL of the product details page.
  * @param {number} props.product.price The price of the product.
- * @param {number} props.product.salesVolume The sales volume of the product.
+ * @param {array}  props.product.tags The tags associated with the product.
  * @example
- * const product = { id: 1, name: 'Apple', imageUrl: 'apple.jpg', price: 1.99, salesVolume: 5000 };
+ * const product = {
+ *   "id": "100-100000910WOF",
+ *   "name": "AMD - Ryzen 7 7800X3D",
+ *   "price": "$449.00",
+ *   "image_url": "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6537/6537139_sd.jpg;maxHeight=192;maxWidth=300",
+ *   "link_url": "https://www.bestbuy.com/site/amd-ryzen-7-7800x3d-8-core-16-thread-4-2-ghz-5-0-ghz-max-boost-socket-am5-unlocked-desktop-processor-black/6537139.p?skuId=6537139",
+ *   "tags": [
+ *     "Advanced Multitasking",
+ *     "High Clock Speed",
+ *     "Gaming and Creation"
+ *   ]
+ *  };
  * return (
  *   <ProductCard product={product} />
  * )
  */
 function ProductCard({ product }) {
   return (
-    <Card className="product-item">
-      <div className="product-item-image-container">
-        <Card.Img variant="top" src={product.imageUrl} alt={product.name} className="product-item-image" />
-        <Button variant="outline-light" className="product-item-add-to-cart-button">
-          <span className="button-icon">+</span>
-        </Button>
-      </div>
-      <Card.Body className="product-item-details">
-        <Card.Title className="product-item-name">{product.name}</Card.Title>
-        <Card.Text className="product-item-price">${product.price.toFixed(2)}</Card.Text>
-        <Card.Text className="product-item-volume">{product.salesVolume}+ sold</Card.Text>
-      </Card.Body>
-    </Card>
+    <a href={product.link_url} className="product-item-link" target="_blank" rel="noopener noreferrer">
+      <Card className="product-item">
+        <div className="product-item-image-container">
+          <Card.Img variant="top" src={product.image_url} alt={product.name} className="product-item-image" />
+        </div>
+        <Card.Body className="product-item-details">
+          <Card.Title className="product-item-name">{product.name}</Card.Title>
+          <Card.Text className="product-item-price">${product.price.toFixed(2)}</Card.Text>
+          <div className="product-item-tags">
+            {product.tags.map((tag, index) => (
+              <Badge key={index} variant="secondary" className="product-item-tag">
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        </Card.Body>
+      </Card>
+    </a>
   );
 }
 
